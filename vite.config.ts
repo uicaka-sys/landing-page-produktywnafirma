fa25f79ce86c6890a701602038c7f5637e8d8e2f
uicaka-sys <uic.aka@gmail.com>
2025-10-25 17:02:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/landing-page-produktywnafirma/",
+  base: '/landing-page-produktywnafirma/',
   server: {
     host: "::",
     port: 8080,
@@ -16,4 +16,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+publicDir: 'public',  
 }));
